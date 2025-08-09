f17c183aa42a56f22aad3ba81a567d32783bd56a
Trulle1234 <leonard.trulsson@gmail.com>
2025-08-09 11:01:41 -0400

    sounds and stuff

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
+const sound = new Map([
+    ["beep", "sound/sfx/beep.wav"],
+    ["phone", "sound/lines/hello.wav"],
+    ["square", "sound/lines/info.wav"],
+]);
+
+function playSound(key) {
+    const audioUrl = sound.get(key);
+    if (!audioUrl) {
+        console.warn(`No audio found for key: ${key}`);
+        return;
+  }
+    const audio = new Audio(audioUrl);
+    audio.play().catch(err => {
+        console.error("Audio play failed:", err);
+  });
+}
+
 const buttons = document.querySelectorAll(".button-grid button");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
+        playSound("beep")
         const img = button.querySelector("img");
         if (img && img.src) {
             const fileName = img.src.split('/').pop().replace('.svg', '');
-            console.log(fileName);
-        }
+            playSound(fileName);''
+        }''
     });
 });
