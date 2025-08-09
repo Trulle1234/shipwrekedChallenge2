14c3b7e9a5f0dd5652415f9e12eb8a25d2d66c35
Trulle1234 <leonard.trulsson@gmail.com>
2025-08-09 09:31:30 -0400

    style and stuff

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("👋🌎");
+const buttons = document.querySelectorAll(".button-grid button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const img = button.querySelector("img");
+        if (img && img.src) {
+            const fileName = img.src.split('/').pop().replace('.svg', '');
+            console.log(fileName);
+        }
+    });
+});
